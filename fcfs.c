60f2ac9b24780fe375a82348437d50e3e0b59eb9
Nav S <navsx@users.noreply.github.com>
2014-08-09 16:34:15 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -4,10 +4,10 @@
 
  struct node    //A node structure is initialized
         {
-		int prio;
+		int pr;
 		int name;
-		int atime;
-		int ctime;
+		int at;
+		int ct;
 		struct node *link;
 	}; 
 	
@@ -16,7 +16,7 @@ void printList(struct node*);
 
 main (int argc,char *argv[])
 {
-	int atmp[5]={};
+	int atmp[4]={};
 	char strtmp[100]="\0";
 	char lstr[100]="\0";
 	char delim[]=",";
@@ -58,7 +58,7 @@ main (int argc,char *argv[])
 
 			        while(ptoken!=NULL)
 				{	
-					for(l=0;l<5;l++)
+					for(l=0;l<4;l++)
 					{				
 				       		//printf("%s\n",ptoken);
 						sscanf(ptoken, "%d", &stoi);
@@ -72,14 +72,20 @@ main (int argc,char *argv[])
 				{
 
 					ptr=first;				
-                                ptr->data=getnumber;
+                                ptr->pr=atmp[0];
+                                ptr->name=atmp[1];
+                                ptr->at=atmp[2];
+                                ptr->at=atmp[3];
 				}
 
-				else if(ptr->data > getnumber)    //If the new number is less then first node => Insert @first
+				else if(ptr->pr > atmp[0])    //If the new number is less then first node => Insert @first
 				{
 					//struct node *temp2=NULL;
 					temp2=(struct node *)malloc(sizeof(struct node));
-					temp2->data=getnumber;
+					temp2->pr=atmp[0];
+	                                temp2->name=atmp[1];
+	                                temp2->at=atmp[2];
+	                                temp2->at=atmp[3];
 					temp2->link=ptr;
 					first=temp2;
 					//ptr->link=NULL;
@@ -88,21 +94,27 @@ main (int argc,char *argv[])
 				else 
 				{
 					//inBetween(ptr->link,getnumber,ptr);
-					while(ptr->data < getnumber)
+					while(ptr->pr < atmp[0])
 					{
 						if(ptr->link==NULL)			//Insert @ last
 						{
 							ptr->link=(struct node *)malloc(sizeof(struct node));
-							ptr->link->data=getnumber;
+							ptr->link->pr=atmp[0];
+							ptr->link->name=atmp[1];
+							ptr->link->at=atmp[2];
+							ptr->link->ct=atmp[3];
 							ptr->link->link=NULL;
 							//printf("\n I am in insert at last\n");
 							exit;
 						}
-						else if(ptr->link!=NULL && ptr->link->data>getnumber)	//Insert @ the middle
+						else if(ptr->link!=NULL && ptr->link->pr>atmp[0])	//Insert @ the middle
 						{
 							//struct node *temp=NULL;
 							temp=(struct node *)malloc(sizeof(struct node));
-							temp->data=getnumber;
+							temp->pr=atmp[0];
+							temp->name=atmp[0];
+							temp->at=atmp[0];
+							temp->ct=atmp[0];
 							temp->link=ptr->link;
 							ptr->link=temp;
 							//temp=NULL;
@@ -135,10 +147,10 @@ void printList(struct node *ptr)
 {
 	struct node *temp3=NULL;
 	temp3=ptr;
-	printf("The contents of the list are : \n");
+	printf("Priority\tpID\tA.T.\tC.T. : \n");
 	while(temp3!=NULL)
 	{
-		printf("%d \n",temp3->data);
+		printf("%d\t,%d\t,%d\t,%d \n",temp3->pr,temp3->name,temp3->at,temp3->ct);
 		temp3=temp3->link;	
 	}
 }	
