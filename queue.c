8cfbaba9e3223c95af81dddb7d429645572cfac0
Nav S <navsx@users.noreply.github.com>
2014-09-04 12:46:30 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -10,28 +10,12 @@ typedef struct node
 	struct node *prev;
 }qu;
 
-/*qu *first=NULL;
-qu *last=NULL;
-qu *ptrl=NULL;
-qu *ptrf=NULL;
-qu *prev=NULL;
-
-first=malloc(sizeof(qu)); 
-first->next=NULL;
-first->prev=NULL;
-ptrf=ptrl=last=first; */
-
-
-int entinQ(int,qu *,qu *,qu *,qu *);
-int delfQ(qu *,qu *,qu *,qu *);
-int display(qu *,qu *);
+int entinQ(int,qu **,qu **,qu **,qu **);
+int delfQ(qu **,qu **,qu **,qu **);
+int display(qu **,qu **);
 
 int main()
 {
-/*	Q *first=*last=*ptrl=NULL;
-	first=(Q*)malloc(sizeof(Q));
-	first->next=NULL;
-	ptrl=last=first; */
 
 	qu *first=NULL;
 	qu *last=NULL;
@@ -54,84 +38,91 @@ int main()
 	{
 		num=rand()%100;
 		printf("%d ",num);
-		entinQ(num,ptrf,ptrl,last,first);
+		entinQ(num,&ptrf,&ptrl,&last,&first);
 	}
 	printf(" ]");
 
 	printf("Queue after insertion is\n");
-	display(ptrf,last);
+	display(&ptrf,&last);
 	
-	num=(rand()%8)+1;
+	num=(rand()%7)+1;
 
 	printf("These numbers will be deleted from the queue\n");
 	printf("[ ");
-	for(i=0;i<num;i++)
+
+	for(i=0;i<4;i++)
 	{
-		delfQ(ptrf,ptrl,last,first);
+		delfQ(&ptrf,&ptrl,&last,&first);
 	}
 	printf(" ]");
-
-	printf("Queue after insertion is\n");
-	display(ptrf,last);
+	
+	printf("Queue after deletion is\n");
+	display(&ptrf,&last);
 
 	return 0;
 }
 
-int entinQ(int num,qu *ptrf,qu *ptrl,qu *last,qu *first)
+int entinQ(int num,qu **ptrf,qu **ptrl,qu **last,qu **first)
 {     
 	//printf("hello");
-	if(ptrl->data == 0)
+	if((*ptrl)->data == 0)
 	{
-		printf("%d\n",ptrl->data);
-		ptrl = first;
-		ptrl->data=num;
-		printf("1st Num ins is =%d\n",ptrl->data);
+		//printf("%d\n",(*ptrl)->data);
+		*ptrl = *first;
+		(*ptrl)->data=num;
+		printf("1st Num ins is =%d\n",(*ptrl)->data);
 		//ptrl->next=(qu *)malloc(sizeof(qu));
 	}
 	else
 	{
-		ptrl->next=(qu *)malloc(sizeof(qu));
-		ptrl->next->data=num;
+		(*ptrl)->next=(qu *)malloc(sizeof(qu));
+		(*ptrl)->next->data=num;
 		//ptrl->next->next=(qu *)malloc(sizeof(qu));
-		ptrl->next->prev=ptrl;
-		ptrl=ptrl->next;
-		ptrl->next=NULL;
-		last=ptrl;
-		printf("other Num ins is =%d\n",ptrl->data);
+		(*ptrl)->next->prev=*ptrl;
+		*ptrl=(*ptrl)->next;
+		(*ptrl)->next=NULL;
+		*last=*ptrl;
+		printf("other Num ins is =%d\n",(*ptrl)->data);
 	}
 	return 0;
 }
 
-int delfQ(qu *ptrf,qu *ptrl,qu *last,qu *first)
+int delfQ(qu **ptrf,qu **ptrl,qu **last,qu **first)
 {
-	if(first==last && first!=NULL)
-	{
-		printf("Deleting Last element\n");
-		first=NULL;
-		ptrl=NULL;
-		last=NULL;
-	}
-	else if(first==last && first==NULL)
-	{
-		printf("Nothing left to delete\n");
-	}
-	else
-	{
-		printf("Deleting..\n");
-		last=last->prev;
-		last->next=NULL;
-		ptrl=last;
-	}
+        if(*first==*last && *first!=NULL)
+        {
+                printf("Deleting Last element\n");
+                *first=NULL;
+                *ptrl=NULL;
+                *last=NULL;
+        }
+        else if(*first==*last && *first==NULL)
+        {
+                printf("Nothing left to delete\n");
+        }
+        else
+        {
+                printf("Deleting..\n");
+     /*           *ptrl=(*ptrl)->prev;
+                (*ptrl)->next=NULL;
+                *last=*ptrl;*/
+
+		printf("%d",(*ptrf)->data);
+                *ptrf=(*ptrf)->next;
+                (*ptrf)->prev=NULL;
+                *first=*ptrf;
+        }
 
 	return 0;
 }
-int display(qu *ptrf,qu *last)
+int display(qu **ptrf,qu **last)
 {
+	qu* temp=*ptrf;
 	printf("[ ");
-	while(ptrf!=last)
+	while(temp!=NULL)
 	{
-		printf("%d ",ptrf->data);
-		ptrf=ptrf->next;
+		printf("%d ",temp->data);
+		temp=temp->next;
 	}
 	printf(" ]");
 	return 0;
