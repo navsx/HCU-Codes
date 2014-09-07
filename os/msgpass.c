4bf3cc26cf07472534edb805383d77608e43232c
Nav S <navsx@users.noreply.github.com>
2014-09-07 08:21:45 +0530

    Update msgpass.c

--- os/msgpass.c
+++ os/msgpass.c
@@ -2,17 +2,30 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
-//I am passing "./<filename> sleep <some value>" as runtime argument
-                       // Ex. ./msgpass sleep 3
+//I am passing "./<filename> sleep <some value> <output file name>" as runtime argument
+                       // Ex. ./msgpass sleep 3 sharefile.txt
 
 int main(int argc,char *argv[])
 {
 	int sum =0,i,slen,pid,stid;
 	time_t t1,t2;
-	float c1,c2;
+	float f,f1;
 	FILE *ifile,*ofile;
+	
+		ofile=fopen(argv[3],"w");
+		if(ofile==0)
+		{
+			printf("in parent: File can't be opened/n");
+			//exit(1);
+		}
+		else
+		{
+			printf("in parent: init file with 1.0 for testing /n");
+			fprintf(ofile,"%f",1.0);
+			fclose(ofile);
+		}
 
-	for(i=1; i<argc; i++) 
+	for(i=1; i<argc-1; i++) 
 	{
 		slen = strlen(argv[i]);
 		sum+=slen;
@@ -31,45 +44,47 @@ int main(int argc,char *argv[])
 		printf("Child will execute- %s\n",s);
 		system(s);
 		time(&t2);
-		c2 = difftime(t2,t1);
+		f1 = difftime(t2,t1);
 		
-		printf("Time taken to execute %s is = %f\n",s,c2);
+		printf("Time taken to execute %s is = %f\n",s,f1);
 
-		f1=fopen("file.txt","w");
-		if(f1==0)
+		ofile=fopen(argv[3],"w");
+		if(ofile==0)
 		{
-			printf("File can't be opened/n");
+			printf("in child:File can't be opened/n");
 			exit(1);
 		}
 		else
 		{
-			fprintf(f1,"%f",c2);
-			fclose(f1);
-
-			printf("Ending time was: %.1f\n",c2);
+			fprintf(ofile,"%f",f1);
+			fclose(ofile);
 		}
 	}
 	else
-	{	waitpid(pid,&stid,0);
-		printf("Parent\n");	
-		c1 = difftime(t1,0); 
-	//	printf("starting time was: %f\n",c1);
-		//printf("\n");
+	{	waitpid(pid,NULL);
+		printf("Parent is executing now \n");	 
 
-		f2=fopen("file.txt","r");
-		if(f2==0)
+		ifile=fopen(argv[3],"r");
+		if(ifile==0)
 		{
-			printf("File can't be opened/n");
+			printf("agian in parent:File can't be opened/n");
 			exit(1);
 		}
 		else
 		{
-			fscanf(f2,"%f",&c2);
-			fclose(f2);
-			printf("Child took %f seconds,Happy!\n",c2);
+			fscanf(ifile,"%f",&f);
+			if(f==1.0)
+			{
+				printf("Child has not written a value yet\n");
+				fclose(ifile);
+			}
+			else
+			{
+				printf("My Child process took %f seconds,I am so happy!\n...but it could've been less\n",f);
+				fclose(ifile);
+			}
 		}
 	}
 
 	return 0;
 }
-
