b0fbf032dae5a93402750c88f92052ebc33096e0
Nav S <navsx@users.noreply.github.com>
2014-09-10 14:51:34 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -2,11 +2,23 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define maxnodes 100
+
+typedef struct node
+{
+	int data;
+	struct node *link;
+}list;
+
 int main(int argc,char *argv[])
 {
 
 	int d1,d2;
 	FILE *ifile,*ofile;
+	char *s[100]=NULL;
+	int nodeArr[maxnodes]={0};
+	list *ptr=NULL;
+	list *first=NULL;
 	
 		ifile=fopen(argv[1],"r");
 		if(ifile==0)
@@ -16,11 +28,41 @@ int main(int argc,char *argv[])
 		}
 		else
 		{
-			while(fgets(s,100,ifile)!=0)
+			while(fgets(s,100,ifile)!=NULL)
                        {
-                        fscanf(ifile,"%d,%d",&d1,&d2);
+                        	sscanf(s,"(%d,%d)",&d1,&d2);
+                        	if(nodeArr[d1]==0)
+                        	{
+                        		ptr=(list *)malloc(sizeof(list));
+                        		ptr->data=d2;
+                        		ptr->link=NULL;
+                        		nodeArr[d1]=ptr;
+                        	}
+                        	else
+                        	{
+                        		ptr=nodeArr[d1];
+                        		while(ptr->link=NULL)
+                        		{
+                        			ptr=ptr->link;
+                        		}
+                        		ptr->link=(list *)malloc(sizeof(list));
+                        		ptr->link->data=d2;
+                        		ptr->link->link=NULL;
+                        	}
+                        	
                        }
-				fclose(ifile);
+			fclose(ifile);
+		}
+		
+		for(i=1;i<=8;i++)
+		{
+			ptr=nodeArr[i];
+			printf("node %d is connected to : /n",nodeArr[i]);
+			while(ptr!=NULL)
+			{
+				printf(" %d ",ptr->data);
+			}
+			printf("\n");
 		}
 
 	return 0;
